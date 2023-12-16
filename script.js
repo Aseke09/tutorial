038ca6d4522f220a5dfb3fd19fe758fc182865fc
Askhat Tassybayev <a.tassybayev@gmail.com>
2023-12-16 16:39:11 +0600

    выполнил задания первого урока

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = "title";
+const screens = "screens";
+const screenPrice = 10;
+const rollback = null;
+const fullPrice = 100;
+const adaptive = true;
+
+alert("Hello");
+console.log("Hello");
